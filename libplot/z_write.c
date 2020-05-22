aa20c0164b2d4f9aff6f4cc0d659d9c841c1cacc
Erin Sheldon <erin.sheldon@gmail.com>
2020-05-22 14:07:20 -0400

    use png_jmpbuf(png_ptr)

--- libplot/z_write.c
+++ libplot/z_write.c
@@ -164,7 +164,7 @@ _pl_z_maybe_output_image (S___(Plotter *_plotter))
     }
 
   /* cleanup after libpng errors (error handler does a longjmp) */
-  if (setjmp (png_ptr->jmpbuf))
+  if (setjmp (png_jmpbuf(png_ptr)))
     {
       png_destroy_write_struct (&png_ptr, (png_info **)NULL);
       return -1;
@@ -444,7 +444,7 @@ _our_error_fn_stdio (png_struct *png_ptr, const char *data)
 #endif
     }
 
-  longjmp (png_ptr->jmpbuf, 1);
+  longjmp (png_jmpbuf(png_ptr), 1);
 }
 
 static void 
